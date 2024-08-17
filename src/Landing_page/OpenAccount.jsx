84f0989ff97833db1cd9546adbd6ff99909b119d
unknown <nk8981398@gmail.com>
2024-08-17 23:33:30 +0530

    pricing page setup

--- src/Landing_page/OpenAccount.jsx
+++ src/Landing_page/OpenAccount.jsx
@@ -5,7 +5,7 @@ const OpenAccount = () => {
     <div className="container mt-5 p-5 mb-5">
       <div className="row text-center p-5">
         <h1 className="fs-2">Open a Zerodha Account</h1>
-        <p className="text-muted mt-4">
+        <p className="text-muted fs-5 mt-4">
           Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
           F&O trades.
         </p>

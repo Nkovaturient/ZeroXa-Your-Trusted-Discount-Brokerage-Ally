d601d8245760d587c90c5de5448e960f8c4df93e
unknown <nk8981398@gmail.com>
2024-08-21 14:10:14 +0530

    dashboard-setup&style

--- src/components/Holdings.jsx
+++ src/components/Holdings.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import { holdings } from "../data/data";
 
 const Holdings = () => {
   return (
-    <div>Holdings</div>
-  )
-}
+    <>
+      <h3
+        className="title"
+        style={{
+          color: "rgb(211, 211, 211)",
+          fontWeight: "600",
+          fontSize: "2.1rem",
+        }}
+      >
+        Holdings ({holdings.length})
+      </h3>
 
-export default Holdings
+      <div className="order-table">
+        <table>
+          <tr>
+            <th>Instrument</th>
+            <th>Qty.</th>
+            <th>Avg. Cost</th>
+            <th>LTP</th>
+            <th>Cur. val.</th>
+            <th>P&L</th>
+            <th>Net Chg.</th>
+            <th>Day Chg.</th>
+          </tr>
+
+          {holdings.map((stock, index) => {
+            const curVal = stock.price * stock.qty;
+            const isProfit = curVal - stock.avg * stock.qty >= 0.0;
+            const profClass = isProfit ? "profit" : "loss";
+            const dayClass = stock.isLoss ? "loss" : "profit";
+
+            return (
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.qty}</td>
+                <td>{stock.avg.toFixed(2)}</td>
+                <td>{stock.price.toFixed(2)}</td>
+                <td>{curVal}</td>
+                <td className={profClass}>
+                  {(curVal - stock.avg * stock.qty).toFixed(2)}
+                </td>
+                <td className={profClass}>{stock.net}</td>
+                <td className={dayClass}>{stock.day}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <h5>
+            29,875.<span>55</span>{" "}
+          </h5>
+          <p>Total investment</p>
+        </div>
+        <div className="col">
+          <h5>
+            31,428.<span>95</span>{" "}
+          </h5>
+          <p>Current value</p>
+        </div>
+        <div className="col">
+          <h5>1,553.40 (+5.20%)</h5>
+          <p>P&L</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Holdings;
--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -1,18 +1,109 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Tooltip, Grow } from "@mui/material";
+import { watchlist } from "../data/data";
+import {
+  BarChartOutlined,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  MoreHoriz,
+} from "@mui/icons-material";
 
 const WatchList = () => {
   return (
     <div className="watchlist-container">
       <div className="search-container">
-        <input type="text" name='search' id='search'
-        className='search'
-        placeholder='Search eg:infy, bse,  nifty fut weekly, gold mcx..' />
-        <span className="counts">9 / 50</span>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="search"
+          placeholder="Search eg:infy, bse,  nifty fut weekly, gold mcx.."
+        />
+        <span className="counts">{watchlist.length} / 50</span>
       </div>
 
-      <ul className="list"></ul>
+      <ul className="list">
+        {watchlist.map((stock, index) => {
+          return <WatchListItem stock={stock} key={index} />;
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default WatchList
+export default WatchList;
+
+const WatchListItem = ({ stock }) => {
+  const [showListItem, setShowListItem] = useState(false);
+
+  const handleMouseEnter = (e) => {
+    setShowListItem(true);
+  };
+
+  const handleMouseLeave = (e) => {
+    setShowListItem(false);
+  };
+
+  return (
+    <>
+      <li onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className="item">
+          <p className={stock.isDown ? "down" : "up"}>{stock.name}</p>
+          <div className="item-info">
+            {" "}
+            {/*//rightend */}
+            <span className="percent">{stock.percent}</span>
+            {stock.isDown ? (
+              <KeyboardArrowDown className="down" />
+            ) : (
+              <KeyboardArrowUp className="up" />
+            )}
+            <span className="price">{stock.price}</span>
+          </div>
+        </div>
+
+        {showListItem && <WatchListActions uid={stock.name} />}
+      </li>
+    </>
+  );
+};
+
+const WatchListActions = ({ uid }) => {
+  return (
+    <span className="actions">
+      <span>
+        <Tooltip
+          title="Buy (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="buy">Buy</button>
+        </Tooltip>
+        <Tooltip
+          title="Sell (B)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="sell">Sell</button>
+        </Tooltip>
+        <Tooltip
+          title="Anaytics (A)"
+          placement="top"
+          arrow
+          TransitionComponent={Grow}
+        >
+          <button className="action">
+            <BarChartOutlined className="icon" />
+          </button>
+        </Tooltip>
+        <Tooltip title="More" placement="top" arrow TransitionComponent={Grow}>
+          <button className="action">
+            <MoreHoriz className="icon" />
+          </button>
+        </Tooltip>
+      </span>
+    </span>
+  );
+};
